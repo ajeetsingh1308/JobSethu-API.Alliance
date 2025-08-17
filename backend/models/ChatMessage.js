d148584426cd88984729f9ae1ad02b860117d38f
Ajeet Singh <ajeetprakashsingh1308@gmail.com>
2025-08-18 01:06:07 +0530

    Delete backend directory
    i have developed better version now, using node+express+supabase

--- backend/models/ChatMessage.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const ChatMessageSchema = new mongoose.Schema({
-    jobId: { type: mongoose.Schema.Types.ObjectId, ref: 'Job', required: true },
-    senderId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    message: { type: String, required: true },
-}, { timestamps: true });
-
-module.exports = mongoose.model('ChatMessage', ChatMessageSchema);
